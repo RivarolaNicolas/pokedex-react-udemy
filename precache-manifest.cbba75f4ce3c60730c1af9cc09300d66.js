b9e7eeb6e55158f84fddfeb24c3a782fe4d2168d
Nicolas Rivarola <RivarolaNicolas94@gmail.com>
2020-10-16 17:45:23 -0300

    Updates

---
+++ precache-manifest.cbba75f4ce3c60730c1af9cc09300d66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eef7bbcadf217976c66ebbc0cb7cda67",
+    "url": "/pokedex-react-udemy/index.html"
+  },
+  {
+    "revision": "28326f27ddc03ac04e57",
+    "url": "/pokedex-react-udemy/static/css/main.17c86894.chunk.css"
+  },
+  {
+    "revision": "23d256d6a325d2ecab45",
+    "url": "/pokedex-react-udemy/static/js/2.746a69a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex-react-udemy/static/js/2.746a69a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28326f27ddc03ac04e57",
+    "url": "/pokedex-react-udemy/static/js/main.2d4e9f9f.chunk.js"
+  },
+  {
+    "revision": "2bf78c079030fdb94caf",
+    "url": "/pokedex-react-udemy/static/js/runtime-main.49ac9298.js"
+  }
+]);
